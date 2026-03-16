221df950be4233d1801f674346729e70b90c9c5e
nz663633 <hyunjisim3@gmail.com>
2026-03-16 17:20:18 +0900

    docs: compare primitive and reference types in methods calls

---
+++ src/ref/VarChange2.java
@@ -0,0 +1,31 @@
+package ref;
+
+public class VarChange2 {
+
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        Data dataB = dataA;
+        // dataA와 dataB는 같은 참조값을 가지게 되고,
+        // 두 변수는 같은 객체 인스턴스를 참조하게 된다
+        // 기본형은 숫자 그 자체를 복사해서 넘겨줌
+        // 참조형은 주소값을 복사해서 넘겨줌
+
+        System.out.println("dataA 참조값 = " + dataA);
+        System.out.println("dataB 참조값 = " + dataB);
+        System.out.println("dataA.value = " + dataA.value);
+        System.out.println("dataB.value = " + dataB.value);
+
+        // dataA 변경
+        dataA.value = 20;
+        System.out.println("변경 dataA.value = 20");
+        System.out.println("dataA.value = " + dataA.value);
+        System.out.println("dataB.value = " + dataB.value);
+
+        // dataB 변경
+        dataB.value = 30;
+        System.out.println("변경 dataB.value = 30");
+        System.out.println("dataA.value = " + dataA.value);
+        System.out.println("dataB.value = " + dataB.value);
+    }
+}
