eb0674db6ceea11e793823e7e227779853638cd2
nz663633 <hyunjisim3@gmail.com>
2026-04-03 09:52:16 +0900

    docs: study Java static variables

---
+++ src/static1/DataCountMain3.java
@@ -0,0 +1,24 @@
+package static1;
+
+public class DataCountMain3 {
+
+    public static void main(String[] args) {
+        Data3 data1 = new Data3("A");
+        System.out.println("A count = " + Data3.count);
+
+        Data3 data2 = new Data3("B");
+        System.out.println("B count = " + Data3.count);
+
+        Data3 data3 = new Data3("C");
+        System.out.println("C count = " + Data3.count);
+
+        // 추가
+        // 인스턴스를 통한 접근 (추천 X)
+        // 인스턴스 변수에 접근한 것처럼 착각할 수 있기 때문에 권장 X
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        // 클래스를 통한 접근
+        System.out.println(Data3.count);
+    }
+}
