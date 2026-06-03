1ecc26a3b269b35453ceb5ac192c66c89fdc8280
nz663633 <hyunjisim3@gmail.com>
2026-06-03 13:35:26 +0900

    refactor: separate exception handling with detailed custom exceptions

--- src/exception/ex3/MainV3.java
+++ src/exception/ex3/MainV3.java
@@ -1,11 +1,14 @@
 package exception.ex3;
 
+import exception.ex3.exception.NetworkServiceV3_2;
+
 import java.util.Scanner;
 
 public class MainV3 {
 
     public static void main(String[] args) {
-        NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        // NetworkServiceV3_1 networkService = new NetworkServiceV3_1();
+        NetworkServiceV3_2 networkService = new NetworkServiceV3_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
