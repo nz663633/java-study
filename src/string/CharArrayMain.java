ad54c743dc3de09c2ecd16a31ae42296b13173b7
nz663633 <hyunjisim3@gmail.com>
2026-06-10 10:31:16 +0900

    feat: study String basics and comparison

---
+++ src/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package string;
+
+public class CharArrayMain {
+
+    public static void main(String[] arg) {
+        char[] charArr = new char[]{'h', 'e', 'l', 'l', 'o'};
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println("str = " + str);
+    }
+}
---
+++ src/string/StringConcatMain.java
@@ -0,0 +1,14 @@
+package string;
+
+public class StringConcatMain {
+
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = " java";
+
+        String result1 = a.concat(b); // x001
+        String result2 = a + b; // x002
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+    }
+}
