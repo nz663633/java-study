19ab28c6c12afc9dd2be4cb45240af03aaa1e27c
nz663633 <hyunjisim3@gmail.com>
2026-06-14 19:42:43 +0900

    feat: solve String method exercises

---
+++ src/string/test/TestString2.java
@@ -0,0 +1,16 @@
+package string.test;
+
+public class TestString2 {
+
+    public static void main(String[] args) {
+        String[] arr = {"hello", "java", "jvm", "spring", "jpa"};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int result = arr[i].length();
+            System.out.println(arr[i] + ": " + result);
+            sum += result;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
