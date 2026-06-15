538931e0d381b7fc6f688ea402f09e26843537b0
nz663633 <hyunjisim3@gmail.com>
2026-06-15 21:20:08 +0900

    feat: solve String method exercises(2)

---
+++ src/string/test/TestString10.java
@@ -0,0 +1,18 @@
+package string.test;
+
+public class TestString10 {
+
+    public static void main(String[] args) {
+        String fruits = "apple,banana,mango";
+
+        // 분리하기
+        String[] parts = fruits.split(",");
+        for (String fruit : parts) {
+            System.out.println(fruit);
+        }
+
+        // 합치기
+        String joined = String.join("->", parts);
+        System.out.println("joined = " + joined);
+    }
+}
---
+++ src/string/test/TestString9.java
@@ -0,0 +1,15 @@
+package string.test;
+
+public class TestString9 {
+
+    public static void main(String[] args) {
+        String email = "hello@example.com";
+
+        String[] parts = email.split("@");
+        String id = parts[0];
+        String domain = parts[1];
+
+        System.out.println("Id = " + id);
+        System.out.println("Domain = " + domain);
+    }
+}
