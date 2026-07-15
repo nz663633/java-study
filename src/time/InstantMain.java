050fc9e3f1edec551e40635e0658584d23d84b16
nz663633 <hyunjisim3@gmail.com>
2026-07-15 23:34:26 +0900

    feat: learn Instant, Duration, and Period

---
+++ src/time/InstantMain.java
@@ -0,0 +1,29 @@
+package time;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
+public class InstantMain {
+
+    public static void main(String[] args) {
+        // 생성
+        Instant now = Instant.now(); // UTC 기준
+        System.out.println("now = " + now);
+
+        ZonedDateTime zdt = ZonedDateTime.now();
+        Instant from = Instant.from(zdt);
+        System.out.println("from = " + from);
+
+        // 1970-01-01 00:00:00(에포크 시간)에서 100초를 더함
+        Instant epochStart = Instant.ofEpochSecond(100);
+        System.out.println("epochStart = " + epochStart);
+
+        // 계산
+        Instant later = epochStart.plusSeconds(3600);
+        System.out.println("later = " + later);
+
+        // 조회
+        long laterEpochSecond = later.getEpochSecond();
+        System.out.println("laterEpochSecond = " + laterEpochSecond);
+    }
+}
