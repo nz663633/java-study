c7041bf0f6d7ad6dedab222ca7ef0a68f49497a0
nz663633 <hyunjisim3@gmail.com>
2026-08-01 19:21:26 +0900

    feat: learn static nested classes

---
+++ src/nested/nested/NestedOuter.java
@@ -0,0 +1,23 @@
+package nested.nested;
+
+public class NestedOuter {
+
+    private static int outClassValue = 3;
+    private int outInstanceValue = 2;
+
+    // 정적 중첩 클래스
+    static class Nested {
+        private int nestedInstanceValue = 1;
+
+        public void print() {
+            // 자신의 멤버에 접근
+            System.out.println(nestedInstanceValue);
+
+            // 바깥 클래스의 인스턴스 멤버에 접근
+            // System.out.println(outInstanceValue);
+
+            // 바깥 클래스의 클래스 멤버에는 접근 가능. private도 접근 가능
+            System.out.println(NestedOuter.outClassValue);
+        }
+    }
+}
